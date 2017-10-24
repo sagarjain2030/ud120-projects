f8e07bb5fc07944fd9ab048b5cfd0ab8dab5efda
sagarjain2030 <sagarjain2030@gmail.com>
2017-10-24 22:15:53 +0530

    This commit includes mini project for Naive Bayes

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -14,20 +14,24 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn.naive_bayes import GaussianNB
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
+t0 = time()
 
 #########################################################
 ### your code goes here ###
-
-
+clf = GaussianNB()
+clf.fit(features_train,labels_train)
 #########################################################
+print "training time:", round(time()-t0, 3), "s"
+print(clf.score(features_test,labels_test))
 
+t1 = time()
+clf.predict(features_test)
+print "predction time = ", round(time() - t1,3),"s"
 
